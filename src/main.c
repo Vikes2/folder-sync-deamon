58d4ba53e066a9fa28d172fa1d40acfcc1ee8eea
wojhan <w.haniewski@gmail.com>
2018-03-20 17:25:30 +0100

    Printing directory content

--- src/main.c
+++ src/main.c
@@ -1,9 +1,29 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+
+void printDirectoryContent(const char* path)
+{
+    struct dirent *entry;
+    int ret = 1;
+    DIR *dir;
+
+    dir = opendir(path);
+    
+    if(dir)
+    {
+        while((entry = readdir(dir)) != NULL)
+        {
+            printf("%s\n", entry->d_name);
+        }
+        closedir(dir); 
+    }
+}
 
 int main(int argc, char** argv)
 {
-    printf("Hello World\n");
+    printDirectoryContent(".");
 
     return 0;
 }
