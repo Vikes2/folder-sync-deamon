5cc2fdc0ca9de7c0dd697e70ec3acbc943d9a9e0
wojhan <w.haniewski@gmail.com>
2018-03-27 14:14:43 +0200

    Start receiving arguments

--- src/main.c
+++ src/main.c
@@ -1,29 +1,51 @@
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
+#include "list.h"
 
-void printDirectoryContent(const char* path)
+void printDirectoryContent(DIR *dir)
 {
     struct dirent *entry;
-    int ret = 1;
-    DIR *dir;
-
-    dir = opendir(path);
-    
-    if(dir)
+    while((entry = readdir(dir)) != NULL)
     {
-        while((entry = readdir(dir)) != NULL)
-        {
-            printf("%s\n", entry->d_name);
-        }
-        closedir(dir); 
+        if(strcmp(entry->d_name, ".") != 0 || strcmp(entry->d_name, "..") != 0)
+        printf("%s -> %d\n", entry->d_name, entry->d_type);
     }
 }
 
 int main(int argc, char** argv)
 {
-    printDirectoryContent(".");
+    char* sourceDirName = argv[1];
+    char* destinationDirName = argv[2];
 
+    DIR *source = opendir(sourceDirName);
+    DIR *dest = opendir(destinationDirName);
+
+    /**
+     
+     * Weryfikacja katalogów
+    
+    if(!source)
+    {
+        perror(sourceDirName);
+        return 1;
+    }
+
+    if(!dest)
+    {
+        perror(destinationDirName);
+        return 1;
+    }
+
+    */
+
+    printDirectoryContent(source);
+
+    closedir(source);
+    closedir(dest);
     return 0;
 }
