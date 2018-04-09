4417261118e7f8c5c2556adcff4379443e761dcf
Vikes2 <muczynski.pawel@gmail.com>
2018-04-09 12:07:04 +0200

    add deleting objects from dest, not adding links

--- src/util.h
+++ src/util.h
@@ -7,8 +7,12 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <utime.h>
+#include <dirent.h>
+#include  <fcntl.h>
+#include "list.h"
 
 char *mergeStrings(char *, char *);
 int syncFilesDate(char*, char*);
+void loadData(List *, DIR *);
 
 #endif
